a8707dd96286328c6679bce0ba48486487ba9b56
hh <hh@emailz.advprog.de>
2019-01-21 23:21:19 +0100

    while Schleife eingefügt

--- ConsoleDezToBin8/Program.cs
+++ ConsoleDezToBin8/Program.cs
@@ -10,6 +10,37 @@ namespace ConsoleDezToBin8
     {
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+
+            Console.WriteLine("ConsoleDezToBin8");
+            Console.WriteLine();
+
+            string strEnde = "";
+            while (strEnde != "e")
+            {
+                Console.Write("Bitte geben Sie eine Zahl im Dezimalsystem ein (0..255): ");
+                string strDez = Console.ReadLine();
+                int nDez = Convert.ToInt32(strDez);
+
+                int nMod = 0;
+                int nDiv = nDez;
+                string strBin = "";
+
+                for (int i = 0; i < 8; i++)
+                {
+                    nMod = nDiv % 2;
+                    nDiv = nDiv / 2;
+                    strBin = nMod + strBin;
+                }
+
+                Console.Write("Die Darstellung im Binärsystem lautet: ");
+                Console.WriteLine(strBin);
+                Console.Write("\nProgramm be(e)nden? ");
+                strEnde = Console.ReadLine();
+            }                    
         }
     }
 }
+
